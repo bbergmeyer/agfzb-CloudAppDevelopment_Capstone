9f11680c1d02835a50a3e855207a64860f05beb0
bbergmeyer <b_bergmeyer@hotmail.com>
2022-04-25 12:24:10 -0500

    update nav bar and deploy settings

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from . import views 
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -10,10 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='/about', view=views.about, name='about'),
+    path(route='about/', view=views.about),
 
     # path for contact us view
-    path(route='/contact', view=views.contact, name='contact'),
+    path(route='contact/', view=views.contact),
 
     # path for registration
 
