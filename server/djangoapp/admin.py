4101a11a4203181fa3c575ef5c5b21e0b28a8f14
bbergmeyer <b_bergmeyer@hotmail.com>
2022-04-28 17:47:39 -0500

    add new models to sqllite

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-# from .models import related models
-from .models import CarMake, CarModel
+from . models import CarMake, CarModel
+
 
 # Register your models here.
 
 # CarModelInline class
-admin.site.register(CarMake)
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
+
+
 # CarModelAdmin class
-admin.site.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'model_type', 'model_year')
+    list_filter = ['model_year']
+    search_fields = ['model_name', 'model_type']
+
+
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ('make_name', 'make_description')
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,8 @@
 import datetime
 from django.db import models
 from django.utils.timezone import now
+import uuid
+import json
 
 
 # Create your models here.
@@ -32,19 +34,19 @@ class CarModel(models.Model):
         (TRUCK, 'Truck')
         ]
     model_type = models.CharField(null=False, max_length=20, choices=model_types, default='SEDAN')
-    model_year = models.DateField(null=False, default=datetime.date.today())
+    model_year = models.DateField(null=False, default=now)
     model_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.model_name + " " + self.model_type + " " + self.model_year 
+        return self.model_name + " " + self.model_type
 
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-class CarDealer(models.Model):
-    car_dealer = models.CharField(null=True, max_length=100)
+#class CarDealer(models.Model):
+#    car_dealer = models.CharField(null=True, max_length=100)
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
-class DealerReview(models.Model):
-    dealer_review = models.CharField(null=True, max_length=100)
+#class DealerReview(models.Model):
+#    dealer_review = models.CharField(null=True, max_length=100)
